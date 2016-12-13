0b4151c35dcec8e37bbd65e54609dbb973bb78ef
nursultan156 <nursultan156@gmail.com>
2016-12-13 13:35:44 +0600

    nodejs.mongodb tests

---
+++ nodejs.mongodb/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by n on 13.12.2016.
+ */
+var repository = require('nodejs.mongodb').repository;
+
+var repository_192_168_66_13 = repository.new('mongodb://192.168.66.13:27017/t1', {server: {socketOptions: {socketTimeoutMS: 1000 * 600}}});
+
+var payload = {
+    collectionName: 'c11',
+    document: {
+        a:1,
+        b:4
+    }
+};
+
+repository_192_168_66_13.createOne(payload, function(err, res){
+    console.log(err);
+    console.log(res);
+});
